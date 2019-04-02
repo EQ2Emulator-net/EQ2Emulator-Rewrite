f3452b3a46e0d57d8ce66f1612fdab44db073a33
theFoof <luchris94@hotmail.com>
2019-04-02 14:09:24 -0400

    Added some missing files

---
+++ trunk/source/common/Packets/EmuPackets/Emu_RegisterZoneServer_Packet.h
@@ -0,0 +1,22 @@
+#include "../../EmuPacket.h"
+#include "../../EmuOpcodes.h"
+#include "../PacketElements/PacketElements.h"
+
+class Emu_RegisterZoneServer_Packet : public EmuPacket {
+public:
+	Emu_RegisterZoneServer_Packet() : EmuPacket(EMUOP_REGISTER_ZONESERVER) {
+		RegisterElements();
+	}
+	~Emu_RegisterZoneServer_Packet() = default;
+
+	void Emu_RegisterZoneServer_Packet::HandlePacket(ZoneStream* z);
+
+	std::string ip;
+	uint16_t port;
+
+private:
+	void RegisterElements() {
+		Register8String(ip);
+		RegisterUInt16(port);
+	}
+};
