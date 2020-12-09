5a4659e7e7eebfcd25e8c050554d78fd5013d89f
Jabantiz <46831793+Jabantiz@users.noreply.github.com>
2020-12-08 19:38:51 -0800

    Fixed the sql for spawn camps

--- trunk/sql/spawn_camp.sql
+++ trunk/sql/spawn_camp.sql
@@ -9,8 +9,8 @@ CREATE TABLE `spawn_camp` (
 	`radius` FLOAT NOT NULL DEFAULT '0',
 	`num_radius_encounters` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `FKZone` (`zone_id`) USING BTREE,
-	CONSTRAINT `FKZone` FOREIGN KEY (`zone_id`) REFERENCES `eq2_rewrite`.`zones` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+	INDEX `FK_spawn_camp_zones` (`zone_id`) USING BTREE,
+	CONSTRAINT `FK_spawn_camp_zones` FOREIGN KEY (`zone_id`) REFERENCES `zones` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
 )
-COLLATE='latin1_swedish_ci'
+COLLATE='latin1_general_ci'
 ENGINE=InnoDB;
